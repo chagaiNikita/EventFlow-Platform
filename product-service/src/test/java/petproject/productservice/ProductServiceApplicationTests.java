9fc6b5ef86019f420f403aaba4744bfa14117fdb
NChagay <NChagay@beeline.kg>
2026-01-26 16:59:45 +0600

    Инициализировал микросервис продуктов, добавил кафку в docker compose

---
+++ product-service/src/test/java/petproject/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package petproject.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

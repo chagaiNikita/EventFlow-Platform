9ce3485546c748a20e113c9a6cdeb4baa81d97bb
NChagay <NChagay@beeline.kg>
2026-01-27 16:51:35 +0600

    Изменил микросервис продуктов на юзер, добавил кафка конфиги

--- user-service/src/test/java/petproject/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/petproject/userservice/UserServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package petproject.productservice;
+package petproject.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductServiceApplicationTests {
+class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
